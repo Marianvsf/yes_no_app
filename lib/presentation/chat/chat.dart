500ca642f2ec3046bd83d1b25534b9bae7c44d9b
Marianvsf <marianvsf@gmail.com>
2025-11-01 15:16:50 -0400

    safe area

--- lib/presentation/chat/chat.dart
+++ lib/presentation/chat/chat.dart
@@ -24,12 +24,15 @@ class _ChatView extends StatelessWidget {
  
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(child: Container(
-          color:Colors.red,
-        ))
-      ],
+    return SafeArea(
+      child: Column(
+        children: [
+          Expanded(child: Container(
+            color:Colors.red,
+          )),
+          Text('Hola')
+        ],
+      ),
     );
   }
 }
