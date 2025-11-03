1f28e22d39eebadc6f0d7b0d2ac817ce3202aa7a
Marianvsf <marianvsf@gmail.com>
2025-11-02 23:56:27 -0400

    image

--- lib/presentation/widgets/chat/him_message_bubble.dart
+++ lib/presentation/widgets/chat/him_message_bubble.dart
@@ -40,6 +40,16 @@ class _ImageBubble extends StatelessWidget {
       child: Image.network("https://yesno.wtf/assets/yes/0-c44a7789d54cbdcad867fb7845ff03ae.gif",
       width: size.width * 0.7,
       height: 150,
-      fit: BoxFit.cover,));
+      fit: BoxFit.cover,
+      loadingBuilder: (context, child, loadingProgress) {
+        if (loadingProgress == null) return child;
+        return Container(
+          width: size.width *0.7,
+          height: 150,
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+          child: Text("cargando"),
+        );
+
+      },));
   }
 }
