5218beb76cf071bf6d8372addfd93488807d16fc
Marianvsf <marianvsf@gmail.com>
2025-11-04 15:11:37 -0400

    stylized

--- lib/presentation/widgets/shared/message_field_box.dart
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -5,21 +5,22 @@ class MessageFieldBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final colors = Theme.of(context).colorScheme;
-
     final outlineInputBorder = UnderlineInputBorder(
-        borderSide: const BorderSide(color: Colors.transparent),
+        borderSide: const BorderSide(color: Color.fromRGBO(0, 0, 0, 0)),
         borderRadius: BorderRadius.circular(40));
 
+    final inputDecoration = InputDecoration(
+      enabledBorder: outlineInputBorder,
+      focusedBorder: outlineInputBorder,
+      filled: true,
+      suffixIcon: IconButton(
+        icon: const Icon(Icons.send),
+        onPressed: () {},
+      ),
+    );
+
     return TextFormField(
-      decoration: InputDecoration(
-          enabledBorder: outlineInputBorder,
-          focusedBorder: outlineInputBorder,
-          filled: true,
-          suffixIcon: IconButton(
-            icon: const Icon(Icons.send),
-            onPressed: () {},
-          )),
+      decoration: inputDecoration,
     );
   }
 }
