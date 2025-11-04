8ac5f593d998212dec06c1eadb798fcfde923c79
Marianvsf <marianvsf@gmail.com>
2025-11-04 14:59:07 -0400

    message field

--- lib/presentation/screens/chat/chat_screen.dart
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:yes_no_app/presentation/widgets/chat/him_message_bubble.dart';
 import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/shared/message_field_box.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -8,22 +9,20 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: const Padding(padding: 
-         EdgeInsets.all(4.0),
-         child: CircleAvatar(
-          backgroundImage: NetworkImage('https://media.diariolasamericas.com/p/5b042d372d43eb53dbc957561341ea51/adjuntos/216/imagenes/100/197/0100197767/adam-levine-afp.jpg'),
-
-         ),
-        ),
-        title: const Text('Adam')),
-        body: _ChatView()
-    );
+        appBar: AppBar(
+            leading: const Padding(
+              padding: EdgeInsets.all(4.0),
+              child: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    'https://media.diariolasamericas.com/p/5b042d372d43eb53dbc957561341ea51/adjuntos/216/imagenes/100/197/0100197767/adam-levine-afp.jpg'),
+              ),
+            ),
+            title: const Text('Adam')),
+        body: _ChatView());
   }
 }
 
 class _ChatView extends StatelessWidget {
- 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -31,13 +30,15 @@ class _ChatView extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 10),
         child: Column(
           children: [
-            Expanded(child: ListView.builder(
-              itemCount: 100,
-              itemBuilder: (context, index){
-              return (index % 2 == 0) ? const HimMessageBubble() :
-              const MyMessageBubble();
-            })),
-            const Text('Hola')
+            Expanded(
+                child: ListView.builder(
+                    itemCount: 100,
+                    itemBuilder: (context, index) {
+                      return (index % 2 == 0)
+                          ? const HimMessageBubble()
+                          : const MyMessageBubble();
+                    })),
+            const MessageFieldBox()
           ],
         ),
       ),
--- lib/presentation/widgets/chat/him_message_bubble.dart
+++ lib/presentation/widgets/chat/him_message_bubble.dart
@@ -5,7 +5,6 @@ class HimMessageBubble extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final colors = Theme.of(context).colorScheme;
 
     return Column(
@@ -13,43 +12,43 @@ class HimMessageBubble extends StatelessWidget {
       children: [
         Container(
           decoration: BoxDecoration(
-            color: colors.secondary,
-            borderRadius: BorderRadius.circular(20)
-          ),
+              color: colors.secondary, borderRadius: BorderRadius.circular(20)),
           child: const Padding(
             padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: Text('Hola', style: TextStyle( color: Colors.white),),
+            child: Text(
+              'Hola',
+              style: TextStyle(color: Colors.white),
+            ),
           ),
-        ), 
-        const SizedBox( height: 5),
+        ),
+        const SizedBox(height: 5),
         _ImageBubble(),
-        const SizedBox( height: 10),
+        const SizedBox(height: 10),
       ],
     );
   }
 }
 
-
 class _ImageBubble extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-  final size = MediaQuery.of(context).size;
+    final size = MediaQuery.of(context).size;
     return ClipRRect(
-      borderRadius: BorderRadius.circular(20),
-      child: Image.network("https://yesno.wtf/assets/yes/0-c44a7789d54cbdcad867fb7845ff03ae.gif",
-      width: size.width * 0.7,
-      height: 150,
-      fit: BoxFit.cover,
-      loadingBuilder: (context, child, loadingProgress) {
-        if (loadingProgress == null) return child;
-        return Container(
-          width: size.width *0.7,
+        borderRadius: BorderRadius.circular(20),
+        child: Image.network(
+          "https://yesno.wtf/assets/yes/0-c44a7789d54cbdcad867fb7845ff03ae.gif",
+          width: size.width * 0.7,
           height: 150,
-          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-          child: const Text("cargando"),
-        );
-
-      },));
+          fit: BoxFit.cover,
+          loadingBuilder: (context, child, loadingProgress) {
+            if (loadingProgress == null) return child;
+            return Container(
+              width: size.width * 0.7,
+              height: 150,
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              child: const Text("Cargando"),
+            );
+          },
+        ));
   }
 }
